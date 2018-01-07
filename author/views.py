3c39492bbba68978bf6ba590ba8b8802e8f12267
Luciano Krebs <luciano.krebs@hotmail.com>
2018-01-07 19:38:12 -0200

    Creating @login_required decorator and implementig a method to track what is the next url the user would like to go

--- author/views.py
+++ author/views.py
@@ -5,6 +5,8 @@ from author.form import RegisterForm, LoginForm
 
 from author.models import Author
 
+from author.decorators import login_required
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
@@ -14,6 +16,9 @@ def login():
     username = data.get('username')
     password = data.get('password')
 
+    if request.method == 'GET' and request.args.get('next'):
+        session['next'] = request.args.get('next', None)
+
     if form.validate_on_submit():
         author = Author.query.filter_by(
             username=username,
@@ -22,13 +27,17 @@ def login():
 
         if author.count():
             session['username'] = username
+            if 'next' in session:
+                next = session.get('next')
+                session.pop('next')
+                return redirect(next)
+
             return(redirect(url_for('login_success')))
 
     return render_template("author/login.html", form=form, error=error)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-
     form = RegisterForm()
     data = request.form
 
@@ -42,5 +51,6 @@ def success():
     return 'Success!'
 
 @app.route('/login_success')
+@login_required
 def login_success():
     return 'login_success'
